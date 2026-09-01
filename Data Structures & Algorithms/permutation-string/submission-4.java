720ebe8753c6d83d6e2b2e63d456ceb7fdf0a13e
Nova Alim <143953646+Nova-Alim@users.noreply.github.com>
2026-09-01 16:07:07 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+
+        HashMap<Character,Integer> word1 = new HashMap<>();
+        HashMap<Character,Integer> word2 = new HashMap<>();
+
+        if(s1==null || s2== null) return false;
+
+        int left=0;
+        int right=0;
+
+      
+      for(int i=0; i<s1.length();i++){
+        word1.put(s1.charAt(i),word1.getOrDefault(s1.charAt(i),0)+1);
+      }
+       
+
+        while(right < s2.length()){
+            word2.put(s2.charAt(right),word2.getOrDefault(s2.charAt(right),0)+1);
+
+            if(((right -left)+1) > s1.length()){
+                word2.put(s2.charAt(left),word2.getOrDefault(s2.charAt(left),0)-1);
+
+                if(word2.get(s2.charAt(left))==0){
+                word2.remove(s2.charAt(left));
+                ;}
+
+                left++;
+            }
+
+            right++;
+
+
+            if(word1.equals(word2)){
+                return true;
+            }
+
+
+        }
+
+
+            return false;
+
+ }
+
+    }
+
