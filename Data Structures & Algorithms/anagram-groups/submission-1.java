e17c68bfd214dbc1fa1a53d2de385789f7fece9d
Nova Alim <143953646+Nova-Alim@users.noreply.github.com>
2026-09-02 17:57:02 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String,List<String>> map = new HashMap<>();
+
+        for(int i=0; i<strs.length;i++){
+
+        int[] count= new int[26];
+
+       
+
+        for(int j=0; j<strs[i].length();j++){
+        
+        count[strs[i].charAt(j)-'a']++;
+        }
+
+        String key = Arrays.toString(count);
+
+        if(map.containsKey(key)){
+            map.get(key).add(strs[i]);
+        }else{
+           List<String> anna = new ArrayList<>();
+           anna.add(strs[i]);
+           map.put(key,anna);
+        }
+        
+    }
+
+    return new ArrayList<>(map.values());
+}
+}
